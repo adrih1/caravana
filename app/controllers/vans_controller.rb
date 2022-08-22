6bdbc2a863a73cd3ba5a283f8e265fe2d5f730c6
Paul Fournier <pfournier597@gmail.com>
2022-08-22 17:51:08 +0200

    controller van created

--- app/controllers/vans_controller.rb
+++ app/controllers/vans_controller.rb
@@ -1,2 +1,36 @@
 class VansController < ApplicationController
+  def index
+    @vans = Van.all
+  end
+
+  # GET /van/1
+  def show
+    @van = Van.find(params[:id])
+    #@review = Review.new
+  end
+
+  # GET /van/new
+  def new
+    @van = Van.new
+  end
+
+  # GET /van/1/edit
+  def edit
+  end
+
+  # POST /van
+  def create
+    @van = Van.new(van_params)
+    if @van.save
+      redirect_to @van, flash: { notice: "Successfully created" }
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def van_params
+    params.require(:van).permit(:description, :color, :location, :price_per_day, :brand, :mileage, :user_id)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_22_141818) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "van_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+    t.index ["van_id"], name: "index_bookings_on_van_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -46,5 +57,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_22_141818) do
     t.index ["user_id"], name: "index_vans_on_user_id"
   end
 
+  add_foreign_key "bookings", "users"
+  add_foreign_key "bookings", "vans"
   add_foreign_key "vans", "users"
 end
