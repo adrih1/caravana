e1750315faa2c1b99fa4c1e4cd8db369cdf2bdd2
Jules Defrancq <julesdefrancq@hotmail.fr>
2022-08-22 17:38:33 +0200

    New & Create in Bookings Controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,21 @@
 class BookingsController < ApplicationController
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+end
+
+private
+
+def booking_params
+  params.require(:booking).permit(:start_date, :end_date)
 end
