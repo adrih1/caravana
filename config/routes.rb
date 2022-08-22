bacdbe4d9384a1bffb560755a95df453caee78f6
Antoine Serval <k6nb4k@gmail.com>
2022-08-22 17:34:11 +0200

    add faker gem, created seed, corrected routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :vans, only[:index, :show, :new, :create, :edit, :destroy] do
-    resources :bookings, only[:new, :create]
+  resources :vans, only: [:index, :show, :new, :create, :edit, :destroy] do
+    resources :bookings, only: [:new, :create]
   end
-  resources :bookings, only[:destroy, :show, :index]
+  resources :bookings, only: [:destroy, :show, :index]
 
   get "dashboard", to: "pages#dashboard"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts "Cleaning DB"
+Van.destroy_all
+User.destroy_all
+puts "DB cleaned"
+
+
+puts 'Creating 15 fake users...'
+15.times do
+  user = User.new({
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    address: Faker::Address.street_name,
+    bio: Faker::Lorem.paragraphs(number: 3).join,
+    age: rand(21..80),
+    driving_license: Faker::IDNumber.spanish_citizen_number,
+    phone_number: Faker::PhoneNumber.phone_number,
+    email: Faker::Internet.email,
+    password: Faker::Code.nric
+  })
+  user.save!
+end
+puts 'Finished!'
+
+puts 'Creating 15 fake vans...'
+15.times do
+  van = Van.new({
+    color:  Faker::Vehicle.color,
+    description: Faker::Lorem.paragraphs(number: 3).join,
+    location:Faker::Address.street_name,
+    brand: Faker::Vehicle.make,
+    price_per_day: rand(50..500),
+    mileage: Faker::Vehicle.mileage,
+    user_id: User.all.sample.id
+  })
+  van.save!
+end
+puts 'Finished!'
