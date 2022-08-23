42c664928549aec65d45f38f0304d834cec5dbdb
Jules Defrancq <julesdefrancq@hotmail.fr>
2022-08-23 17:58:55 +0200

    Cloudinary en cours

--- app/models/van.rb
+++ app/models/van.rb
@@ -1,5 +1,6 @@
 class Van < ApplicationRecord
   has_many :bookings
+  has_one_attached :photo
   belongs_to :user
   validates :description, presence: true, length: { minimum: 20 }
   validates :location, presence: true
