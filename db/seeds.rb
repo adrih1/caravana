b29c399ad168bf9319cab7c5b723a1bc64358fe7
Adrien Hors <adrienhors1@gmail.com>
2022-08-24 13:23:03 +0200

    DashBoard Booking Display - good

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,16 +16,16 @@ puts "DB cleaned"
 puts 'Creating 15 fake users...'
 15.times do
   user = User.new({
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    address: Faker::Address.street_name,
-    bio: Faker::Lorem.paragraphs(number: 3).join,
-    age: rand(21..80),
-    driving_license: Faker::IDNumber.spanish_citizen_number,
-    phone_number: Faker::PhoneNumber.phone_number,
-    email: Faker::Internet.email,
-    password: Faker::Code.nric
-  })
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      address: Faker::Address.street_name,
+      bio: Faker::Lorem.paragraphs(number: 3).join,
+      age: rand(21..80),
+      driving_license: Faker::IDNumber.spanish_citizen_number,
+      phone_number: Faker::PhoneNumber.phone_number,
+      email: Faker::Internet.email,
+      password: Faker::Code.nric
+    })
   user.save!
 end
 puts 'Finished!'
@@ -34,7 +34,7 @@ puts 'Creating 15 fake vans...'
 15.times do
   van = Van.new({
     title: Faker::Company.bs,
-    color:  Faker::Vehicle.color,
+    color: Faker::Vehicle.color,
     capacity: rand(2..8),
     description: Faker::Lorem.paragraphs(number: 3).join,
     location: Faker::Address.street_name,
@@ -42,7 +42,7 @@ puts 'Creating 15 fake vans...'
     price_per_day: rand(50..500),
     mileage: Faker::Vehicle.mileage,
     user_id: User.all.sample.id
-    })
+  })
 
   file1 = URI.open("https://source.unsplash.com/random/?trucks")
   van.photos.attach(io: file1, filename: "file1.jpg", content_type: "image.jpg")
