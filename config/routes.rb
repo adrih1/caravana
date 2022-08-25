47fe504b7ce524ccd865f045b40af60bd8518f5a
Paul Fournier <pfournier597@gmail.com>
2022-08-25 14:09:07 +0200

    mise en place des reviews

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   resources :vans, only: [:index, :show, :new, :create, :edit, :destroy] do
     resources :bookings, only: [:create]
   end
-  resources :bookings, only: [:destroy, :show, :index]
+  resources :bookings, only: [:destroy, :show, :index] do
+    resources :reviews
+  end
 
   get "dashboard", to: "pages#dashboard"
   get "my_vans", to: "pages#my_vans"
