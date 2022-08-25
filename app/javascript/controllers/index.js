35afee38d96ff8d9b1fd4939149737265581c471
Antoine Serval <k6nb4k@gmail.com>
2022-08-25 10:59:56 +0200

    des trucs

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,14 +4,11 @@
 
 import { application } from "./application"
 
-import AddressAutocompleteController from "./address_autocomplete_controller"
-application.register("address-autocomplete", AddressAutocompleteController)
+import LocationAutocompleteController from "./location_autocomplete_controller"
+application.register("location-autocomplete", LocationAutocompleteController)
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
 import MapController from "./map_controller"
 application.register("map", MapController)
-
-import NavbarController from "./navbar_controller"
-application.register("navbar", NavbarController)
